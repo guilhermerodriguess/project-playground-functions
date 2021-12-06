0b9559eaac72f7f64b8b464e91a5c1731f39f35f
Guilherme R <grsguiga@gmail.com>
2021-12-06 20:47:26 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,9 +20,73 @@ function techList(tech,name) {
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let ddd = []
+  let cincoPrimeiros = []
+  let quatroUltimos = []
+  let numRepetido = 0
+
+  function numberComplete(numbers) {
+      for (number in numbers) {  
+      
+          if (number < 2) {
+          ddd.push(numbers[number])
+          }
+          else if (number < 7) {
+          cincoPrimeiros.push(numbers[number])
+          }
+          else if (number < 11) {
+          quatroUltimos.push(numbers[number])
+          }
+      }
+      ddd = ddd.join('')
+      cincoPrimeiros = cincoPrimeiros.join('')
+      quatroUltimos = quatroUltimos.join('')
+  
+      let numeroGerado = `(${ddd}) ${cincoPrimeiros}-${quatroUltimos}`
+      return numeroGerado
+  }
+  
+  function verificaRepetido(numbers) {
+      for (let number of numbers) {
+          numRepetido = 0
+          for (let number2 in numbers) {
+              
+              if (number === numbers[number2]) {
+                  numRepetido ++
+                  if (numRepetido >= 3) {
+                      return true
+                  }
+              }
+              
+          }
+      
+      }
+  }
+  
+  function numberCorrect(numbers) {
+      for (let number in numbers)
+      if (numbers[number] < 0 || numbers[number] > 9) {
+          return true 
+      }
+  }
+
+  if (numbers.length !== 11) {
+          return "Array com tamanho incorreto."
+  }
+  else if (numberCorrect(numbers) === true) {
+      return "não é possível gerar um número de telefone com esses valores"
+  }
+  else if (verificaRepetido(numbers) === true) {
+      return "não é possível gerar um número de telefone com esses valores"
+  }
+  else {
+      return numberComplete(numbers)
+  }
+  
 }
+
+numbers = []
 
 // Desafio 12
 function triangleCheck() {
